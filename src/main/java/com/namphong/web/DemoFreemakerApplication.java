e23309a88b7d469376c35fcbc624b64c6cee4945
quangblackhat <quangtk@bap.jp>
2019-04-23 10:29:49 +0700

    Add extends SpringBootServletInitializer

--- src/main/java/com/namphong/web/DemoFreemakerApplication.java
+++ src/main/java/com/namphong/web/DemoFreemakerApplication.java
@@ -2,9 +2,10 @@ package com.namphong.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DemoFreemakerApplication {
+public class DemoFreemakerApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoFreemakerApplication.class, args);
